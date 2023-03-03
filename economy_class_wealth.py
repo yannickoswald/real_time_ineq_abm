f1a3c02d58fe106739e7c1f8becc28aacca28463
Yannick Oswald <y-oswald@web.de>
2023-03-03 14:27:34 +0000

    smaller changes

--- economy_class_wealth.py
+++ economy_class_wealth.py
@@ -91,7 +91,7 @@ class Economy():
                     scale_coeff =  410400/5.26
                     a_wealth = powerlognorm.rvs(0.33, 1.15, size=1)*scale_coeff
                 elif self.start_year == 1990:
-                    scale_coefft = 203900/5.26
+                    scale_coeff = 203900/5.26
                     a_wealth = powerlognorm.rvs(1.05, 1.6, size=1)*scale_coeff
             ## create agent
             agents.append(WealthAgent(i, a_wealth, self, self.economy_beta))
--- run_economy.py
+++ run_economy.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan 31 10:58:26 2023
-
 @author: earyo
 """
-
 import os
 import pandas as pd
 import sys
 import numpy as np
 import random
 os.chdir(".")
-
 import matplotlib.pyplot as plt
 from economy_class_wealth import Economy
 from inequality_metrics import find_wealth_groups
@@ -19,35 +16,16 @@ from inequality_metrics import find_wealth_groups
 #%%
 ### LOAD empirical monthly wealth Data
 with open('./data/wealth_data_for_import.csv') as f:
-    data = pd.read_csv(f, encoding = 'unicode_escape')
-#%%    
-### PLOT empirical monthly wealth Data
-wealth_groups = ["Top 1%", "Top 10%", "Middle 40%", "Bottom 50%"]
-fig, ax = plt.subplots()
-
-for g in wealth_groups: 
-
-    x = data["date_short"][data["group"] == g].reset_index(drop = True)
-    y = data["real_wealth_share"][data["group"] == g].reset_index(drop = True)
-    
-    ax.plot(x,y, label = g)
-
-ax.set_xticks(x.iloc[0::50].index)
-ax.set_xticklabels(x.iloc[0::50], rotation = 90)
-ax.legend(frameon = False)
-ax.set_ylim((-0.05, 1))
-ax.margins(0)
+    d1 = pd.read_csv(f, encoding = 'unicode_escape')
 #%%
 
 economy = Economy(1000, 0.025, 1, "Pareto_lognormal", 1990)
 ### one-time procedure
 economy.make_agents()
 list_agents = economy.agents
-
 array_agent_wealth = np.asarray([x.wealth for x in economy.agents])
 plt.hist(array_agent_wealth, bins = 100)
 plt.show()
-
 
 data = []
 time_horizon = 100
@@ -73,3 +51,18 @@ plt.legend()
 plt.show()
     
 plt.hist([x.wealth for x in economy.agents])
+
+#%%    
+### PLOT empirical monthly wealth Data (01/1990 to 12/2018)
+wealth_groups = ["Top 1%", "Top 10%", "Middle 40%", "Bottom 50%"]
+fig, ax = plt.subplots()
+for g in wealth_groups: 
+    x = d1["date_short"][d1["group"] == g].reset_index(drop = True).iloc[168:516]
+    y = d1["real_wealth_share"][d1["group"] == g].reset_index(drop = True).iloc[168:516]   
+    ax.plot(x,y, label = g)
+x = x.reset_index(drop=True)
+ax.set_xticks(x.iloc[0::20].index)
+ax.set_xticklabels(x.iloc[0::20], rotation = 90)
+ax.legend(frameon = False, bbox_to_anchor=(-0.7, -0.4, 1., .102))
+ax.set_ylim((-0.05, 1))
+ax.margins(0)
