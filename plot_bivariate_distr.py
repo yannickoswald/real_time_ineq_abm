b5bff6abe775e3b6b3376159a034d0711614ab3c
Yannick Oswald <y-oswald@web.de>
2023-05-05 16:14:37 +0100

    first tests to plots the macro state vector properly
    fct with and without chatgpt

--- plot_bivariate_distr.py
+++ plot_bivariate_distr.py
@@ -5,11 +5,7 @@ Created on Fri May  5 15:16:47 2023
 @author: earyo
 """
 
-
-
-
 ### https://python.quantecon.org/kalman.html
-
 import matplotlib.pyplot as plt
 plt.rcParams["figure.figsize"] = (11, 5)  #set default figure size
 from scipy import linalg
@@ -20,26 +16,15 @@ from scipy.stats import norm
 from scipy.integrate import quad
 from numpy.random import multivariate_normal
 from scipy.linalg import eigvals
+import pandas as pd
 
-data = [2.58399e+06,
-        2.83755e+06,
-        3.57959e+06,
-        2.49093e+06,
-        1.94883e+06,
-        3.79383e+06,
-        3.27399e+07,
-        1.22652e+06,
-        2.6629e+06,	
-        6.49244e+06, 
-        897820,	
-        1.50362e+06,
-        3305e+06,
-        939202,	
-        1.15138e+06,	
-        1.33823e+06,	
-        4.32165e+06,	710621,	1.04543e+06,	1.44985e+06, 155583,	179718,	242988,	135442,	194931,	155338,	175074,	132494,	147125,	201740, 15439,	18842.2,	24072.5,	17953.8,	15820.6,	17579.3,	16602.9,	17827.5,	23460.1,	29584.1]
+with open('./data/test_bivariate_distr.csv') as f:
+            data = pd.read_csv(f, encoding = 'unicode_escape', header = None)    
+            
+x = np.array(data.iloc[0,:])
+y = np.array(data.iloc[3,:])
 
-
+"""
 # Set up the Gaussian prior density p
 Σ = [[0.4, 0.3], [0.3, 0.45]]
 Σ = np.matrix(Σ)
@@ -56,63 +41,33 @@ Q = 0.3 * Σ
 y = np.matrix([2.3, -1.9]).T
 
 # Set up grid for plotting
-x_grid = np.linspace(-1.5, 2.9, 100)
-y_grid = np.linspace(-3.1, 1.7, 100)
+x_grid = np.linspace(min(x), max(x), 100)
+y_grid = np.linspace(min(y), max(y), 100)
 X, Y = np.meshgrid(x_grid, y_grid)
 
-def bivariate_normal(x, y, σ_x=1.0, σ_y=1.0, μ_x=0.0, μ_y=0.0, σ_xy=0.0):
-    """
-    Compute and return the probability density function of bivariate normal
-    distribution of normal random variables x and y
+"""
 
-    Parameters
-    ----------
-    x : array_like(float)
-        Random variable
 
-    y : array_like(float)
-        Random variable
 
-    σ_x : array_like(float)
-          Standard deviation of random variable x
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
 
-    σ_y : array_like(float)
-          Standard deviation of random variable y
+def plot_bivariate_normal(a, b):
+    n = len(a)
+    mean = [np.mean(a), np.mean(b)]
+    cov = np.cov(a, b)
+    x, y = np.meshgrid(np.linspace(np.min(a), np.max(a), n), np.linspace(np.min(b), np.max(b), n))
+    pos = np.empty(x.shape + (2,))
+    pos[:, :, 0] = x
+    pos[:, :, 1] = y
+    rv = multivariate_normal(mean=mean, cov=cov)
+    fig, ax = plt.subplots()
+    ax.contourf(x, y, rv.pdf(pos))
+    ax.set_xlabel('a')
+    ax.set_ylabel('b')
+    ax.set_title('Bivariate Normal Distribution')
+    plt.show()
 
-    μ_x : scalar(float)
-          Mean value of random variable x
 
-    μ_y : scalar(float)
-          Mean value of random variable y
-
-    σ_xy : array_like(float)
-           Covariance of random variables x and y
-
-    """
-
-    x_μ = x - μ_x
-    y_μ = y - μ_y
-
-    ρ = σ_xy / (σ_x * σ_y)
-    z = x_μ**2 / σ_x**2 + y_μ**2 / σ_y**2 - 2 * ρ * x_μ * y_μ / (σ_x * σ_y)
-    denom = 2 * np.pi * σ_x * σ_y * np.sqrt(1 - ρ**2)
-    return np.exp(-z / (2 * (1 - ρ**2))) / denom
-
-def gen_gaussian_plot_vals(μ, C):
-    "Z values for plotting the bivariate Gaussian N(μ, C)"
-    m_x, m_y = float(μ[0]), float(μ[1])
-    s_x, s_y = np.sqrt(C[0, 0]), np.sqrt(C[1, 1])
-    s_xy = C[0, 1]
-    return bivariate_normal(X, Y, s_x, s_y, m_x, m_y, s_xy)
-
-# Plot the figure
-
-fig, ax = plt.subplots(figsize=(10, 8))
-ax.grid()
-
-Z = gen_gaussian_plot_vals(x_hat, Σ)
-ax.contourf(X, Y, Z, 6, alpha=0.6, cmap=cm.jet)
-cs = ax.contour(X, Y, Z, 6, colors="black")
-ax.clabel(cs, inline=1, fontsize=10)
-
-plt.show()
+plot_bivariate_normal(x, y)
