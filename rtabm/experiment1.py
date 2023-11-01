ec01d0bd3f55b7c6f6dbab2405d299eb6a1c50a6
Yannick Oswald <y-oswald@web.de>
2023-11-01 17:35:48 +0000

    toy experiment changes

---
+++ rtabm/experiment1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  1 17:20:14 2023
+
+@author: earyo
+"""
+
--- rtabm/test_example_computation_ENKF.py
+++ rtabm/test_example_computation_ENKF.py
@@ -5,53 +5,49 @@ Created on Mon Oct 23 11:25:36 2023
 @author: earyo
 """
 
-
-import os
-import pandas as pd
-import sys
-import numpy as np
-import random
 #%% 
-
-
 
 import random
 import numpy as np
 
 class KalmanToy():
 
-    def __init__(self, mean, std_dev):
+    def __init__(self, mean, std_dev, rich_agents=None, rich_agents_obs=None):
         self.mean = mean
         self.std_dev = std_dev
-        self.agent_data = self.create_agent_states()
-        self.obs_ensemble = self.create_obs_ensemble()
+        self.agent_data = None  # specified through create_agent_state
+        self.obs_ensemble = None  # specified through create_agent_state
         self.current_obs = [150, 11]
-        self.current_obs_var = np.var(self.obs_ensemble, axis=1)
+        self.current_obs_var = None  # to be set later
         self.H = np.array([[1, 0, 0], [0, 0.5, 0.5]])
-    
-    def create_agent_states(self):
-        rich_agents = list()
-        for i in range(3):
-            random_number = random.gauss(self.mean, self.std_dev)
-            clipped_number = min(200, max(100, random_number))    
-            rich_agents.append(clipped_number)  
-        agents = np.array([rich_agents, [10, 15, 5], [5, 5, 5]]) 
-        return agents 
+        self.create_agent_states(rich_agents)  # Populate self.agent_data
+        self.create_obs_ensemble(rich_agents_obs)  # Populate self.obs_ensemble
+        self.current_obs_var = np.var(self.obs_ensemble, axis=1)  # Now we can set this
 
-    def create_obs_ensemble(self): 
-        rich_agents_obs = list()
-        for i in range(3):
-            random_number = random.gauss(self.mean, self.std_dev)
-            clipped_number = min(200, max(100, random_number))    
-            rich_agents_obs.append(clipped_number) 
-        obs_ensemble = np.array([rich_agents_obs, [10, 11, 9]])
-        return obs_ensemble
+    def create_agent_states(self, rich_agents=None):
+        if rich_agents is None:
+            rich_agents = []
+            for i in range(3):
+                random_number = random.gauss(self.mean, self.std_dev)
+                clipped_number = min(200, max(100, random_number))    
+                rich_agents.append(clipped_number)  
+        self.agent_data = np.array([rich_agents, [10, 15, 5], [5, 5, 5]])
 
+    def create_obs_ensemble(self, rich_agents_obs=None): 
+        if rich_agents_obs is None:
+            rich_agents_obs = []
+            for i in range(3):
+                random_number = random.gauss(self.mean, self.std_dev)
+                clipped_number = min(200, max(100, random_number))    
+                rich_agents_obs.append(clipped_number) 
+        self.obs_ensemble = np.array([rich_agents_obs, [10, 11, 9]])
+        
     def make_data_covariance(self, data):
         return np.diag(data)
 
     def make_gain_matrix(self):
         C = np.cov(self.agent_data)
+        self.current_obs_var = np.var(self.obs_ensemble, axis=1)
         data_covariance = self.make_data_covariance(self.current_obs_var)
         state_covariance = self.H @ C @ self.H.T
         diff = state_covariance + data_covariance
@@ -63,16 +59,17 @@ class KalmanToy():
         diff = self.obs_ensemble - self.H @ self.agent_data
         X = self.agent_data + Kalman_Gain @ diff
         Y = self.H @ X
-        return diff, X, Y
+        return diff, X, Y, Kalman_Gain @ diff
 
+# Usage without specifying rich_agents and rich_agents_obs
+kalman_toy1 = KalmanToy(150, 20)
+print("Without custom rich_agents and rich_agents_obs")
+print("Agent Data:", kalman_toy1.agent_data)
+print("Observation Ensemble:", kalman_toy1.obs_ensemble)
 
-# Usage
-kalman_toy = KalmanToy(150, 20)
-Kalman_Gain, state_covariance, C = kalman_toy.make_gain_matrix()
-diff, X, Y = kalman_toy.state_update()
-agent_states = kalman_toy.agent_data
-obs_ensemble = kalman_toy.obs_ensemble
-print("diff:", diff)
-print("X:", X)
-print("Y:", Y)
-
+# Usage with specifying rich_agents and rich_agents_obs
+kalman_toy2 = KalmanToy(150, 20, [150, 170, 130], [110, 185, 150])
+data2 = kalman_toy2.state_update()
+print("\nWith custom rich_agents and rich_agents_obs")
+print("Agent Data:", kalman_toy2.agent_data)
+print("Observation Ensemble:", kalman_toy2.obs_ensemble)
